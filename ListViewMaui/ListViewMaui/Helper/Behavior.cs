4bffcb0102919338daf9ce878af071951042bab6
Suja Venkatesan <suja.venkatesan@syncfusion.com>
2023-01-11 19:41:36 +0530

    MAUI-4054 : How to sort the items by datetime property along with Grouping in .Net Maui (SfListview)

---
+++ ListViewMaui/ListViewMaui/Helper/Behavior.cs
@@ -0,0 +1,52 @@
+﻿using ListViewMaui.Helper;
+using Microsoft.Maui.Controls;
+using Syncfusion.Maui.DataSource;
+using Syncfusion.Maui.DataSource.Extensions;
+using Syncfusion.Maui.ListView;
+using Syncfusion.Maui.ListView.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListViewMaui
+{
+    public class Behavior:Behavior<ContentPage>
+    {
+        #region Fields
+
+        private Syncfusion.Maui.ListView.SfListView listView;
+       
+        #endregion
+
+        #region Overrides
+        protected override void OnAttachedTo(ContentPage bindable)
+        {
+            listView = bindable.FindByName<Syncfusion.Maui.ListView.SfListView>("listView");
+            listView.DataSource.GroupDescriptors.Add(new GroupDescriptor()
+            {
+                PropertyName = "DateOfBirth",
+                KeySelector = (object obj1) =>
+                {
+                    var item = (obj1 as Contacts);
+                    return item.DateOfBirth.Year;
+                },
+            });
+
+            this.listView.DataSource.SortDescriptors.Add(new SortDescriptor()
+            {
+                PropertyName = "DateOfBirth",
+                Direction = ListSortDirection.Ascending
+            });
+            base.OnAttachedTo(bindable);
+        }
+        protected override void OnDetachingFrom(ContentPage bindable)
+        {
+            listView = null;
+            base.OnDetachingFrom(bindable);
+        }
+        #endregion
+    }
+}
---
+++ ListViewMaui/ListViewMaui/Helper/CustomGroupComparer.cs
@@ -0,0 +1,38 @@
+﻿using Syncfusion.Maui.DataSource.Extensions;
+using Syncfusion.Maui.DataSource;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListViewMaui.Helper
+{
+    class CustomGroupComparer : IComparer<GroupResult>, ISortDirection
+    {
+        public CustomGroupComparer()
+        {
+            this.SortDirection = ListSortDirection.Ascending;
+        }
+
+        public ListSortDirection SortDirection
+        {
+            get;
+            set;
+        }
+
+        public int Compare(GroupResult x, GroupResult y)
+        {
+            DateTime xvalue = Convert.ToDateTime(x.Key);
+            DateTime yvalue = Convert.ToDateTime(y.Key);
+
+            // Group results are compared and return the SortDirection
+            if (xvalue.CompareTo(yvalue) > 0)
+                return SortDirection == ListSortDirection.Ascending ? 1 : -1;
+            else if (xvalue.CompareTo(yvalue) == -1)
+                return SortDirection == ListSortDirection.Ascending ? -1 : 1;
+            else
+                return 0;
+        }
+    }
+}
